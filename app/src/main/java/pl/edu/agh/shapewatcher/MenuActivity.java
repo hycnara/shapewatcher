d7f804eda82c6190dfdc0d817c833293912c27d8
hycnara <hycnar.agnieszka@yahoo.com>
2017-10-23 21:04:39 +0200

    New empty activities added to menu

--- app/src/main/java/pl/edu/agh/shapewatcher/MenuActivity.java
+++ app/src/main/java/pl/edu/agh/shapewatcher/MenuActivity.java
@@ -1,8 +1,13 @@
 package pl.edu.agh.shapewatcher;
 
+import android.app.AlertDialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -47,21 +52,34 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         if(v == buttonNewGame){
-
+            startActivity(new Intent(MenuActivity.this, NewGameActivity.class));
         }
         if(v == buttonStatistics){
-
+            startActivity(new Intent(MenuActivity.this, StatisticsActivity.class));
         }
         if(v == buttonRanking){
-
+            startActivity(new Intent(MenuActivity.this, RankingActivity.class));
         }
         if(v == buttonSettings){
-
+            startActivity(new Intent(MenuActivity.this, SettingsActivity.class));
         }
         if(v == buttonLogOut){
-            firebaseAuth.signOut();
-            finish();
-            startActivity(new Intent(MenuActivity.this, WelcomeActivity.class));
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Log out")
+                    .setMessage("Are you sure you want to log out?")
+                    .setPositiveButton("YES", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            firebaseAuth.signOut();
+                            finish();
+                            startActivity(new Intent(MenuActivity.this, WelcomeActivity.class));
+                        }
+                    })
+                    .setNegativeButton("NO", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {}
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
         }
     }
 }
